88b3023b0afe452992a1bdd58ceb975539a03c55
Gurqu1 <gurpreetgs19881@gmail.com>
2025-06-07 13:49:36 -0700

    made navbar stick to the top

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     }
   }
   return (
-    <nav className="flex justify-between items-center border-2 min-w-full min-h-20 rounded-md pl-3 pr-3">
+    <nav className="flex justify-between items-center border-2 min-w-full min-h-20 rounded-md pl-3 pr-3 sticky top-0">
         <div className='text-2xl font-semibold'>Rock Paper Scissors</div>
         <div className='flex justify-between gap-3 min-w-38'>
             <button className='flex items-center border-2 max-h-10 rounded-sm text-base p-3'>Rankings</button>
