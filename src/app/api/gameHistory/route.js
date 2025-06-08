42f9a6d64b8f00f4c924fb6421069d77ca7a2b3b
Gurqu1 <gurpreetgs19881@gmail.com>
2025-06-08 14:56:37 -0700

    created api route for game history

---
+++ src/app/api/gameHistory/route.js
@@ -0,0 +1,52 @@
+// https://nextjs.org/docs/app/building-your-application/routing/route-handlers#request-body
+// https://supabase.com/docs/reference/javascript/introduction
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function GET() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    return Response.json({ error: 'Error fetching dummy data' }, { status: 500 });
+  }
+  const user = data.user;
+
+  const { gameHistory, fetchError } = await supabase.from('game_history').select('*').eq('user_id', user.id);
+  if (fetchError) {
+    return Response.json({ fetchError: 'Error fetching dummy data' }, { status: 500 });
+  }
+
+  return Response.json(gameHistory);
+}
+
+export async function POST(request) {
+  const res = await request.json();
+
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    return Response.json({ error: 'Error fetching dummy data' }, { status: 500 });
+  }
+  const user = data.user;
+
+  const { data: dummyData, error: dummyError } = await supabase.from('dummy').insert({
+    user_id: user.id,
+    text: res.text,
+  });
+
+  if (dummyError) {
+    return Response.json({ error: 'Error creating dummy data' }, { status: 500 });
+  }
+
+  return Response.json(dummyData);
+}
+
+// export async function PUT(request) {
+
+// }
+
+// export async function DELETE(request) {
+
+// }
