78ff51dbdfff428bfd3ddd5b052aca1faf165072
Gurqu1 <gurpreetgs19881@gmail.com>
2025-06-08 22:52:14 -0700

    fully working GET POST DELETE api methods for player_stats table

--- src/app/api/playerStats/route.js
+++ src/app/api/playerStats/route.js
@@ -6,11 +6,11 @@ import { createClient } from '@/utils/supabase/server'
 export async function GET() {
   const supabase = await createClient();
 
-  const { data: data, error: gameError } = await supabase.auth.getUser();
-  if (error || !data?.user) {
+  const { data: userData, error: userError } = await supabase.auth.getUser();
+  if (userError || !userData?.user) {
     return Response.json({ error: 'User not logged in' }, { status: 500 });
   }
-  const user = data.user;
+  const user = userData.user;
 
   const { data: playerStats, error: fetchError } = await supabase.from('player_stats').select('*').eq('user_id', user.id);
   if (fetchError) {
@@ -24,15 +24,15 @@ export async function POST(request) {
 
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
+  const { data: userData, error: userError } = await supabase.auth.getUser();
+  if (userError || !userData?.user) {
     return Response.json({ error: 'User not logged in' }, { status: 500 });
   }
-  const user = data.user;
+  const user = userData.user;
 
-  const { data: result, error: entryError } = await supabase.from('player_stats').eq('user_id', user.id)
-  if (entryError) {
-    const { data: result, error: savingError } = await supabase.from('player_stats').insert({
+  const { data: result, error: entryError } = await supabase.from('player_stats').select('*').eq('user_id', user.id)
+  if (!result || result.length === 0) {
+    const { data: postingResult, error: savingError } = await supabase.from('player_stats').insert({
         user_id: user.id,
         username: res.username,
         total_games: res.total_games,
@@ -41,12 +41,12 @@ export async function POST(request) {
         draws: res.draws,
         score: res.score,
         current_win_streak: res.current_win_streak,
-        best_wind_streak: res.best_wind_streak,
+        best_win_streak: res.best_win_streak,
       })
       if (savingError) {
         return Response.json({ error: 'Error creating player stats data' }, { status: 500 });
       }
-      return Response.json(result);
+      return Response.json({msg: "inserted player stats", postingResult});
   } else {
     const { data: updatedResult, error: updateError } = await supabase.from('player_stats').update({
       username: res.username,
@@ -56,20 +56,30 @@ export async function POST(request) {
       draws: res.draws,
       score: res.score,
       current_win_streak: res.current_win_streak,
-      best_wind_streak: res.best_wind_streak,
+      best_win_streak: res.best_win_streak,
     }).eq('user_id', user.id);
 
     if (updateError) {
       return Response.json({ error: 'Error updating player stats data' }, { status: 500 });
     }
-    return Response.json(updatedResult);
+    return Response.json({msg: "updated player stats", updatedResult});
   }
 }
 
-// export async function PUT(request) {
+export async function DELETE(request) {
+    const res = await request.json();
+    
+    const supabase = await createClient();
 
-// }
-
-// export async function DELETE(request) {
-
-// }
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+    if (userError || !userData?.user) {
+      return Response.json({ error: 'User not logged in' }, { status: 500 });
+    }
+    const user = userData.user;
+    
+    const { data: result, error: deleteError } = await supabase.from('player_stats').delete().eq('user_id', user.id);
+    if (deleteError) {
+        return Response.json({ error: 'Error deleting player stats data' }, { status: 500 });
+    }
+    return Response.json(result);
+}
