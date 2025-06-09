60d6aec973e33e0e71e5b3f691c4d1496fdb4555
Xale121 <doggoes.arf@gmail.com>
2025-06-09 15:32:20 -0700

    update the rankings page to fit the style, and make the button work

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,9 +2,11 @@
 'use client'
 import React, { useState } from 'react'
 import ProfileCard from './profileCard'
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
   let [displayProfile, setDisplayProfile] = useState('hidden')
+  const router = useRouter();
   function toggleProfile() {
     if (displayProfile === 'hidden'){
       setDisplayProfile('flex')
@@ -13,11 +15,14 @@ const Navbar = () => {
     setDisplayProfile('hidden')
     }
   }
+  function goToRankings() {
+    router.push('/rankings')
+  }
   return (
     <nav className="flex justify-between items-center border-2 min-w-full min-h-20 rounded-md pl-3 pr-3">
         <div className='text-2xl font-semibold'>Rock Paper Scissors</div>
         <div className='flex justify-between gap-3 min-w-38'>
-            <button className='flex items-center border-2 max-h-10 rounded-sm text-base p-3'>Rankings</button>
+            <button className='flex items-center border-2 max-h-10 rounded-sm text-base p-3'onClick={goToRankings}>Rankings</button>
             <button className='flex items-center border-2 max-h-10 rounded-sm text-base p-3' onClick={toggleProfile}>Profile</button>
             <ProfileCard displayType={displayProfile}/>
         </div>
