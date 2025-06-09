44902600c747981fb06eac2154bd870b7a8999e3
Xale121 <doggoes.arf@gmail.com>
2025-06-09 14:38:28 -0700

    comment explaining .order

--- src/app/api/allPlayerStats/route.js
+++ src/app/api/allPlayerStats/route.js
@@ -7,6 +7,8 @@ export async function GET() {
   const { data: playerStats, error: fetchError } = await supabase
   .from('player_stats')
   .select('*')
+  // sort by the score using supabase function .order
+  // https://supabase.com/docs/reference/javascript/order
   .order('score', { ascending: false });
   if (fetchError) {
     return Response.json({ fetchError: 'Error fetching player stats data' }, { status: 500 });
