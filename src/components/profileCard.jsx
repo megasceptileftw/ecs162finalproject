37afacd41a818e2d54f53dd14655d859de1a9d3f
Xale121 <doggoes.arf@gmail.com>
2025-06-09 21:54:09 -0700

    made the stats in the dropdown accurate

--- src/components/profileCard.jsx
+++ src/components/profileCard.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ProfileCard = ({displayType}) => {
-    let classType = `${displayType} flex-col justify-center absolute mt-18 border-2 p-6`
-    async function logout() {
-      let res = await fetch('/auth/signout', {
-        method: 'POST',
-      })
-      if (res.ok) {
-        window.location.href = '/login'
-      }
+const ProfileCard = ({ displayType }) => {
+  let classType = `${displayType} flex-col justify-center absolute mt-18 border-2 p-6`
+
+  const [stats, setStats] = useState()
+  useEffect(() => {
+    fetch('/api/playerStats')
+      .then((res) => res.json())
+      .then((data) => setStats(data[0]))
+  }, [])
+  if (!stats) return null
+
+
+  async function logout() {
+    let res = await fetch('/auth/signout', {
+      method: 'POST',
+    })
+    if (res.ok) {
+      window.location.href = '/'
     }
+  }
+
   return (
     <div className={classType}>
-        <img src="https://newprofilepic.photo-cdn.net//assets/images/article/profile.jpg?90af0c8" className='self-center rounded-full max-w-20'></img>
-        <div className='self-center'>Stats: 100</div>
-        <div className='self-center'>W/L Ratio: 50/100</div>
-        <div className='self-center'>Wins: 50</div>
-        <div className='self-center'>Losses: 50</div>
-        <button className='self-center display: block border rounded-sm mb-2 p-1 text-sm'>Match History</button>
-        <button className='self-center display: block border rounded-sm p-1 text-sm text-red-600' onClick={logout}>Logout</button>
+      <img src="https://newprofilepic.photo-cdn.net//assets/images/article/profile.jpg?90af0c8" className="self-center rounded-full max-w-20"/>
+      <div className="self-center">Score: {stats.score}</div>
+      <div className="self-center">Total: {stats.total_games}</div>
+      <div className="self-center">Wins: {stats.wins}</div>
+      <div className="self-center">Losses: {stats.losses}</div>
+      <div className="self-center">Draws: {stats.draws}</div>
+      <button className="self-center border rounded-sm mb-2 p-1 text-sm">Match History</button>
+      <button className="self-center border rounded-sm p-1 text-sm text-red-600" onClick={logout}>Logout</button>
     </div>
   )
 }
